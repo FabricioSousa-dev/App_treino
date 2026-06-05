0c99715c196e20acafb9b671600dc3b79b62bd90
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-06-05 19:11:13 -0300

    feat: implement loop-based name validation and exit command in user input flow

--- user.py
+++ user.py
@@ -1,6 +1,9 @@
-u = str(input("Digite o seu nome: "))
-opcao = " "
-while not u.strip() or not u.isalpha():
-    print("Nome invalido,use letras!")
-    u = str(input("Digite o seu nome: "))
-print("Olá {}!".format(u))
+while True:
+    nome = str(input("Digite seu nome:  ")).strip().upper()
+    if nome == "FECHAR":
+        print("fechando!....")
+        break
+    while not nome.isalpha():
+        print("Por favor, digite apenas letras")
+        nome = str(input("Digite seu nome:  ")).strip().upper()
+print(f"Bem-vindo {nome}")
