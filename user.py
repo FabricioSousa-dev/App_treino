829e1fd89f1fa23b9aa7b53cf8c8253b91a64848
Fabricio Teixeira <ffabricioteixeira15@gmail.com>
2026-07-01 18:43:00 -0300

    Implement user registration and input validation
    Refactor user input handling and add user registration functionality.

--- user.py
+++ user.py
@@ -1,9 +1,20 @@
+usuarios = list()
+
 while True:
-    nome = str(input("Digite seu nome:  ")).strip().upper()
-    if nome == "FECHAR":
-        print("fechando!....")
+    u = str(input("Digite o seu nome: "))
+
+    opcao = " "
+    while not u.strip() or not u.isalpha():
+        print("Nome invalido,use letras!")
+        u = str(input("Digite o seu nome: "))
+
+    print("Olá {}!".format(u))
+    opcao = str(input("Deseja cadastrar outro usuario? (s/n): "))
+
+    if opcao.lower() != "s":
         break
-    while not nome.isalpha():
-        print("Por favor, digite apenas letras")
-        nome = str(input("Digite seu nome:  ")).strip().upper()
-print(f"Bem-vindo {nome}")
+print("Usuario cadastrado com sucesso!")
+print("Usuarios cadastrados:")
+
+for i, user in enumerate(usuarios,start=1):
+    print(f"{i} {user}")
