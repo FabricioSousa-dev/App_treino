aba83903374d28c6e11ef1c193a685e5538669b3
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-07-15 15:25:24 -0300

    refactor: improve user input flow and structure in user.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 
 
-import user
 
-def main():
-    user.main()
+    
--- user.py
+++ user.py
@@ -1,24 +1,23 @@
-user = dict()
 person = list()
 
-
-
 while True:
-        user['Nome'] = input("Enter your name: ").upper().strip()
-        user['Sobrenome'] = input("Enter your last name: ").upper().strip()
-        user['Idade'] = input("Enter your age: ").strip()
-        user['Peso'] = input("Enter your weight: ").strip()
-        user['Altura'] = input("Enter your height: ").strip()
+    user = dict()
+    user['Nome'] = input("Enter your name: ").upper().strip()
+    user['Sobrenome'] = input("Enter your last name: ").upper().strip()
+    user['Idade'] = input("Enter your age: ").strip()
+    user['Peso'] = input("Enter your weight: ").strip()
+    user['Altura'] = input("Enter your height: ").strip()
 
-        opc = ''
-        while opc not in 'SN':
-               opc = input("Do you want to continue? [S/N] ").upper().strip()[0]
-        if opc == 'N':
-               break
-        person.append(user.copy()) 
+    while True:
+        opc = input("Do you want to add another person? [S/N]: ").upper().strip()[0]
+        if opc in 'SN':
+            break
+        print("Error! Please enter only S or N.")
 
-
+    person.append(user.copy())
+    if opc == 'N':
+        break
 
 print("Position   | Name | Last Name | Age | Weight | Height")
 for k, v in enumerate(person):
-      print(f"{k+1:<10} | {v['Nome']:<4} | {v['Sobrenome']:<9} | {v['Idade']:<3} | {v['Peso']:<6} | {v['Altura']:<6}")
+    print(f"{k+1:<10} | {v['Nome']:<4} | {v['Sobrenome']:<9} | {v['Idade']:<3} | {v['Peso']:<6} | {v['Altura']:<6}")
