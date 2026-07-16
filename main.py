0084c550687d5107d0a0b8bbc319437c8eb5783e
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-07-16 12:35:59 -0300

    feat: refactor main function into exibir_menu for improved user interaction

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
+import os
 from app.models import criar_tabelas, adicionar_usuario
 
-def main():
-    print(f"Bem-vindo(a) {__name__}!")
-    print("Iniciando o sistemas...")
-    criar_tabelas()
-    print("Tabelas criadas com sucesso!")
-
-    print("Adicionando usuário...")
-    adicionar_usuario("João", 25)
-    print("Usuário adicionado com sucesso!")
-
-if __name__ == '__main__':
-    main()
+def exibir_menu():
+    print("\n + ""=" * 30)
+    print("🏋️  SISTEMA APP TREINO 🏋️")
+    print("="*30)
+    print("1.criar tabelas")
