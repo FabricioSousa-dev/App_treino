d8efa8b60860f7b84f6a00e76d43dc22ccc9270d
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-07-16 14:25:52 -0300

    fix: correct syntax error in exibir_menu and clean up user input functions

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def exibir_menu():
     print("1.criar tabelas")
     print("2.Listar usuários cadastrados")
     print("0.Sair do sistema")
-    print("="30)
+    print("="*30)
+
 
 def limpar_tela():
     os.system('cls')
+
+def main():
+    criar_tabelas()
+    limpar_tela()
