d22f47864876e483c498486d82b99f4d1a340b22
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-07-16 14:03:14 -0300

    fix: correct import statement and enhance user table structure in database

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app.Database import get_connection
+from app.database import get_connection
 
 def criar_tabelas():
     conn = get_connection()
@@ -6,32 +6,39 @@ def criar_tabelas():
 
 
     cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS users (
-                       id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nome TEXT NOT NULL,
-                   idade integer
-                   )
-                   ''')
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            sobrenome TEXT NOT NULL,
+            idade INTEGER,
+            peso REAL,
+            altura REAL
+        )
+    ''')
     
     cursor.execute('''
-            CREATE TABLE IF NOT EXISTS exercises (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   USER_id INTEGER NOT NULL,
-                   series INTEGER,
-                   FOREIGN KEY (USER_id) REFERENCES users(id)
-                   )
-                   ''')
+        CREATE TABLE IF NOT EXISTS exercises (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            nome_exercicio TEXT NOT NULL,
+            series INTEGER,
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        )
+    ''')
+    
     conn.commit()
     conn.close()
 
 
-def adicionar_usuario(nome, idade):
+def adicionar_usuario(nome, sobrenome, idade, peso, altura):
     conn = get_connection()
     cursor = conn.cursor()
-
-    cursor.execute('INSERT INTO users (nome, idade) VALUES (?,?)', (nome, idade))
-
+    
+    
+    cursor.execute('''
+        INSERT INTO users (nome, sobrenome, idade, peso, altura) 
+        VALUES (?, ?, ?, ?, ?)
+    ''', (nome, sobrenome, idade, peso, altura))
+    
     conn.commit()
     conn.close()
-
-
--- main.py
+++ main.py
@@ -12,29 +12,3 @@ def exibir_menu():
 
 def limpar_tela():
     os.system('cls')
-
-def main():
-    criar_tabelas()
-
-    limpar_tela()
-    print("Bem-vindo ao sistema de treino!")
-
-    while True:
-        exibir_menu()
-
-        opcao = input("Escolha uma opção: ")
-
-        if opcao == "1":
-            limpar_tela()
-            print("========Cadastrar novo usuário========")
-            nome = input("Digite o nome do usuário: ")
-            idade = input("Digite a idade do usuário: ")
-
-            if idade.isdigit():
-                adicionar_usuario(nome, idade)
-                print(f"Usuário {nome} cadastrado com sucesso!")
-            else:
-                print("\n❌ Erro: a idade deve ser um número inteiro. por favor,tente novamente.")
-        elif opcao == "2":
-            limpar_tela()
-            print("========Listar usuários cadastrados========")
